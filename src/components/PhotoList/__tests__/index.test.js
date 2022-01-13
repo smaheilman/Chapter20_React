976dfe46ef263b1e2a9b5b91ffbff2ccb8583bdd
Shmee <smaheilman@gmail.com>
2022-01-12 23:27:06 -0500

    finish modal

--- src/components/PhotoList/__tests__/index.test.js
+++ src/components/PhotoList/__tests__/index.test.js
@@ -1,12 +1,11 @@
-// __tests__/PhotoList.test.js
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import PhotoList from './PhotoList'
+import PhotoList from '..'
 
 afterEach(cleanup)
 
-describe('PhotoList is rendering', () => {
+describe('PhotoList component', () => {
   it('renders', () => {
     render(<PhotoList />);
   });
